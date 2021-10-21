bb6a8d93f45db0f664de1eb314dab68d17d7f53b
Abin <porag214@gmail.com>
2021-10-21 13:23:59 +0600

    AddScoped

--- Microservice.Customer/Startup.cs
+++ Microservice.Customer/Startup.cs
@@ -25,6 +25,7 @@ namespace Microservice.Customer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
             services.AddControllers();
         }
 
