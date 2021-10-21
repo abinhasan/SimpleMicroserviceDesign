42d9db7a290c75bd5e002eaf3154174514096cdb
Abin <porag214@gmail.com>
2021-10-21 13:19:45 +0600

    add ProductRepository

---
+++ Microservice.Product/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microservice.Product
+{
+    public interface IProductRepository
+    {
+        public Task<List<Microservice.Product.Model.Product>> GetAllProducts();
+    }
+}
---
+++ Microservice.Product/ProductRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microservice.Product
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly List<Microservice.Product.Model.Product> products = new List<Microservice.Product.Model.Product>();
+        public ProductRepository()
+        {
+            products.Add(new Microservice.Product.Model.Product
+            {
+                Id = Guid.NewGuid(),
+                Code = "P0001",
+                Name = "Lenovo Laptop",
+                Quantity_In_Stock = 15,
+                Unit_Price = 125000
+            });
+
+            products.Add(new Microservice.Product.Model.Product
+            {
+                Id = Guid.NewGuid(),
+                Code = "P0002",
+                Name = "DELL Laptop",
+                Quantity_In_Stock = 25,
+                Unit_Price = 135000
+            });
+
+            products.Add(new Microservice.Product.Model.Product
+            {
+                Id = Guid.NewGuid(),
+                Code = "P0003",
+                Name = "HP Laptop",
+                Quantity_In_Stock = 20,
+                Unit_Price = 115000
+            });
+        }
+        public Task<List<Microservice.Product.Model.Product>> GetAllProducts()
+        {
+            return Task.FromResult(products);
+        }
+    }
+}
