350fb6670320563d1a0b2f03aabc351bdedc6e5a
Abin <porag214@gmail.com>
2021-10-21 13:17:57 +0600

    add Product

---
+++ Microservice.Product/Model/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microservice.Product.Model
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public int Quantity_In_Stock { get; set; }
+        public decimal Unit_Price { get; set; }
+    }
+}
