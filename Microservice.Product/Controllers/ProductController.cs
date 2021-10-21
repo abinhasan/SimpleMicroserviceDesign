9b4b35df5ed97c8ec9839aa394dd93cbf7fa7243
Abin <porag214@gmail.com>
2021-10-21 13:22:28 +0600

    add ProductController

---
+++ Microservice.Product/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microservice.Product.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+        public ProductController(IProductRepository customerRepository)
+        {
+            _productRepository = customerRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Microservice.Product.Model.Product>>> GetAllCustomers()
+        {
+            return await _productRepository.GetAllProducts();
+        }
+    }
+}
